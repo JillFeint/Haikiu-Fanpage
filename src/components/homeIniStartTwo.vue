eb498e18d41f838a7c5770c5a18843ee7a9ebd90
JuanB47 <jucabapa39@gmail.com>
2024-04-21 19:44:15 -0500

    solucin error

--- src/components/homeIniStartTwo.vue
+++ src/components/homeIniStartTwo.vue
@@ -177,9 +177,9 @@ export default {
   padding: 20px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
-.equip-container--all{
-  /* background-color: blue; */
-}
+/* .equip-container--all{
+  background-color: blue;
+} */
 .titleAsqueroso {
   display: flex;
   background-color: rgb(53, 54, 68);
